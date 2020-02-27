f02dbf5f39cf92cc09d5f35f467cefbf7d1cdb5e
Holger <hdecarne@gmail.com>
2020-02-27 06:48:56 +0100

    Add macho provider (fragment)

---
+++ src/main/java/de/carne/filescanner/provider/macho/MachOFormat.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2007-2020 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.filescanner.provider.macho;
+
+import java.io.IOException;
+
+import de.carne.filescanner.engine.FileScannerResult;
+import de.carne.filescanner.engine.FileScannerResultDecodeContext;
+import de.carne.filescanner.engine.spi.Format;
+
+/**
+ * <a href="https://en.wikipedia.org/wiki/Mach-O">Mach-O executable file format</a> decoder.
+ */
+public class MachOFormat extends Format {
+
+	/**
+	 * Format name.
+	 */
+	public static final String FORMAT_NAME = "Mach-O image";
+
+	private final MachOFormatSpecDefinition formatSpecDefinition;
+
+	/**
+	 * Constructs a new {@linkplain MachOFormat} instance.
+	 */
+	public MachOFormat() {
+		super(FORMAT_NAME);
+		this.formatSpecDefinition = new MachOFormatSpecDefinition();
+		this.formatSpecDefinition.load();
+		registerHeaderSpec(this.formatSpecDefinition.headerSpec());
+	}
+
+	@Override
+	public FileScannerResult decode(FileScannerResultDecodeContext context) throws IOException {
+		return context.decodeComposite(this.formatSpecDefinition.formatSpec());
+	}
+
+}
---
+++ src/main/java/de/carne/filescanner/provider/macho/MachOFormatSpecDefinition.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2007-2020 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.filescanner.provider.macho;
+
+import java.net.URL;
+import java.util.Objects;
+
+import de.carne.filescanner.engine.format.spec.CompositeSpec;
+import de.carne.filescanner.engine.format.spec.DWordSpec;
+import de.carne.filescanner.engine.format.spec.FormatSpecDefinition;
+import de.carne.filescanner.engine.util.IntHelper;
+import de.carne.util.Lazy;
+
+/**
+ * See MachO.formatspec
+ */
+final class MachOFormatSpecDefinition extends FormatSpecDefinition {
+
+	@Override
+	protected URL getFormatSpecResource() {
+		return Objects.requireNonNull(getClass().getResource("MachO.formatspec"));
+	}
+
+	private Lazy<CompositeSpec> machoFormatSpec = resolveLazy("MACHO_FORMAT", CompositeSpec.class);
+	private Lazy<CompositeSpec> machHeaderSpec = resolveLazy("MACH_HEADER_64", CompositeSpec.class);
+
+	private Lazy<DWordSpec> cmdSize = resolveLazy("CMD_SIZE", DWordSpec.class);
+
+	public CompositeSpec formatSpec() {
+		return this.machoFormatSpec.get();
+	}
+
+	public CompositeSpec headerSpec() {
+		return this.machHeaderSpec.get();
+	}
+
+	public Long cmdSize() {
+		return IntHelper.toUnsignedLong(this.cmdSize.get().get()) - 8;
+	}
+
+}
